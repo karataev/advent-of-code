7aba215f17d379c05e97ec7b462ca3f5f2fa663b
karataev <eugene.karataev@gmail.com>
2017-12-08 21:51:48 +0700

    Add day8/2017

---
+++ 2017/day8/day8-2.js
@@ -0,0 +1,55 @@
+// const data = `b inc 5 if a > 1
+// a inc 1 if b < 5
+// c dec -10 if a >= 1
+// c inc -20 if c == 10`;
+const data = require('./day8-data');
+
+function getRegisters(instructions) {
+    let result = {};
+    instructions
+        .forEach(item => {
+            const register = item.split(' ')[0];
+            result[register] = 0;
+        });
+    return result;
+}
+
+function parseInstruction(registers, item) {
+    const parts = item.split(' ');
+    const register = parts[0];
+    const operation = parts[1];
+    const amount = isNaN(parts[2]) ? registers[[parts[2]]] : Number(parts[2]);
+    const reg2 = registers[parts[4]];
+    const condition = eval(`${reg2}${parts[5]}${parts[6]}`); // is evil!
+    if (!condition) return;
+    if (operation === 'inc') {
+        registers[register] += amount;
+    } else if (operation === 'dec') {
+        registers[register] -= amount;
+    }
+}
+
+function getMaxValue(obj) {
+    let max = -Infinity;
+    const keys = Object.keys(obj);
+    keys.forEach(key => {
+        if (obj[key] > max) max = obj[key];
+    });
+    return max;
+}
+
+function evaluate(data) {
+    const instructions = data.split('\n');
+    let registers = getRegisters(instructions);
+    let max = 0;
+
+    instructions.forEach(item => {
+        parseInstruction(registers, item);
+        const currentMax = getMaxValue(registers);
+        if (Math.abs(currentMax) > max) max = Math.abs(currentMax);
+    });
+
+    console.log(max);
+}
+
+evaluate(data);
